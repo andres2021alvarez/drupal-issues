dd396ca13a5c04787efa21dc573f8e6e5b5b00fa
andres alvarez <andres.alvarez@esinergia.co>
2024-09-18 11:22:38 -0500

    phpstan

--- drupal-issues/phpstan.sh
+++ drupal-issues/phpstan.sh
@@ -1,3 +1,3 @@
-drupal-issues/vendor/bin/phpcbf --standard=Drupal,DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml web/modules/contrib/pathauto
+./vendor/bin/phpcbf --standard=Drupal,DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml web/modules/contrib/pathauto
 
 ./vendor/bin/phpcs --standard=Drupal,DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml web/modules/contrib/pathauto
