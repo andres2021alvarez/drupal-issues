9ed380b39ce15b59e3b1a0567b1faac428719ad4
andres alvarez <andres.alvarez@esinergia.co>
2024-09-24 20:22:39 -0500

    update

--- drupal-issues/web/phpcs.sh
+++ drupal-issues/web/phpcs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if [ "$1" == "fix" ]; then
-  ./vendor/bin/phpcbf --standard=Drupal,DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml docroot/sites/terceros/modules/custom/fsfb_terceros_form/src/Controller/uniqueEmail.php
-else
-  ./vendor/bin/phpcs --standard=Drupal,DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml docroot/sites/terceros/modules/custom/fsfb_terceros_form/src/Controller/uniqueEmail.php
-fi
+  ./vendor/bin/phpcbf --standard=Drupal,DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml web/modules/contrib/restui
+
+  ./vendor/bin/phpcs --standard=Drupal,DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml web/modules/contrib/restui
+
